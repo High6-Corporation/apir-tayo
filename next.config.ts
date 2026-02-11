ad1cfd02f2f5349cdad461233b09530785e5fced
VPS Server <developer.mode111@gmail.com>
2026-02-11 13:19:51 +0000

    Minor changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+          { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
